9c7b4c85f9dfcb3cacf576d56f322e73385f7315
Lydia <lydia.cupery@atomicobject.com>
2023-12-02 12:08:23 -0500

    day 2 ✨🎄

---
+++ 2023/day2/cube_conundrum/test.ts
@@ -0,0 +1,13 @@
+import { calculateSolution } from "./solution";
+
+describe.only("cube_conundrum", () => {
+  it("should add up ids that are valid", () => {
+    const input = `Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green`;
+    const result = calculateSolution(input);
+    expect(result).toEqual(8);
+  });
+});
