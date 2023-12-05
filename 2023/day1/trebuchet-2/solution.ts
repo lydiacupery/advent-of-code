899910982ea69531fa85a39258897c5f0806da32
Lydia <lydia.cupery@atomicobject.com>
2023-12-04 19:33:24 -0500

    cleanup

--- 2023/day1/trebuchet-2/solution.ts
+++ 2023/day1/trebuchet-2/solution.ts
@@ -1,20 +1,3 @@
-/**
- * The newly-improved calibration document consists of lines of text; each line originally contained a specific calibration value that the Elves now need to recover. On each line, the calibration value can be found by combining the first digit and the last digit (in that order) to form a single two-digit number.
- * 
- * Your calculation isn't quite right. It looks like some of the digits are actually spelled out with letters: one, two, three, four, five, six, seven, eight, and nine also count as valid "digits".
-
-Equipped with this new information, you now need to find the real first and last digit on each line. For example:
-
-two1nine
-eightwothree
-abcone2threexyz
-xtwone3four
-4nineeightseven2
-zoneight234
-7pqrstsixteen
-In this example, the calibration values are 29, 83, 13, 24, 42, 14, and 76. Adding these together produces 281.
- */
-
 const textToDigitMap: Record<string, string> = {
   one: "1",
   two: "2",
